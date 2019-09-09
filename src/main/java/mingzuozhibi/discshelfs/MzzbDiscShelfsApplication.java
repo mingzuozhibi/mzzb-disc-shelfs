405af90ccbc280636906a9fec1f07837c865591e
fuhaiwei <fuhaiwei@126.com>
2019-09-09 19:43:57 +0800

    feat: Add From SpringBoot

---
+++ src/main/java/mingzuozhibi/discshelfs/MzzbDiscShelfsApplication.java
@@ -0,0 +1,13 @@
+package mingzuozhibi.discshelfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzzbDiscShelfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MzzbDiscShelfsApplication.class, args);
+	}
+
+}
