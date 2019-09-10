f0cb92ea94499bf712b1bef1f016883043b8e94f
fuhaiwei <fuhaiwei@126.com>
2019-09-10 09:25:52 +0800

    feat: Setup Spring Config

---
+++ manual/initial_db.sql
@@ -0,0 +1,4 @@
+# DROP DATABASE IF EXISTS mzzb_disc_shelfs_pro;
+DROP DATABASE IF EXISTS mzzb_disc_shelfs_dev;
+CREATE DATABASE IF NOT EXISTS mzzb_disc_shelfs_pro CHARSET utf8;
+CREATE DATABASE IF NOT EXISTS mzzb_disc_shelfs_dev CHARSET utf8;
