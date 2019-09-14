70362ccb4f7f05b07e77c001e5fcfc0a8d28baf4
fuhaiwei <fuhaiwei@126.com>
2019-09-15 01:03:03 +0800

    feat: Use Common 0.2.7

--- src/main/java/mingzuozhibi/discshelfs/DiscShelfSpider.java
+++ src/main/java/mingzuozhibi/discshelfs/DiscShelfSpider.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import mingzuozhibi.common.jms.JmsMessage;
 import mingzuozhibi.common.model.Result;
 import mingzuozhibi.common.spider.SpiderRecorder;
+import mingzuozhibi.discshelfs.DiscShelfTaskBuilder.DiscShelfTask;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -11,22 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static mingzuozhibi.common.spider.SpiderCdp4j.*;
 import static mingzuozhibi.common.spider.SpiderRecorder.writeContent;
-import static mingzuozhibi.discshelfs.DiscShelfSpiderSupport.buildTaskUrls;
+import static mingzuozhibi.discshelfs.DiscShelfTaskBuilder.buildTasks;
 
 @Slf4j
 @Service
 public class DiscShelfSpider {
-
-    private static class SpiderContext {
-        private AtomicInteger errorCount = new AtomicInteger(0);
-        private AtomicInteger fetchCount = new AtomicInteger(0);
-        private AtomicInteger doneCount = new AtomicInteger(0);
-        private AtomicInteger discCount = new AtomicInteger(0);
-    }
 
     @Autowired
     private JmsMessage jmsMessage;
@@ -34,19 +27,17 @@ public class DiscShelfSpider {
     private DiscShelfRepository discShelfRepository;
 
     public void fetchFromAmazon() {
-        List<String> taskUrls = buildTaskUrls();
-        SpiderRecorder recorder = new SpiderRecorder("上架信息", taskUrls.size(), jmsMessage);
-        AtomicInteger rowCount = new AtomicInteger(0);
+        List<DiscShelfTask> tasks = buildTasks();
+        SpiderRecorder recorder = new SpiderRecorder("上架信息", tasks.size(), jmsMessage);
         recorder.jmsStartUpdate();
 
         doInSessionFactory(factory -> {
-            for (String taskUrl : taskUrls) {
+            for (DiscShelfTask task : tasks) {
                 if (recorder.checkBreakCount(5)) break;
-                recorder.jmsStartUpdateRow("*");
+                recorder.jmsStartUpdateRow(task.getOrigin());
 
-                String origin = "page=" + recorder.getFetchCount();
-                Result<String> bodyResult = waitResult(factory, taskUrl);
-                if (recorder.checkUnfinished(bodyResult, origin)) {
+                Result<String> bodyResult = waitResult(factory, task.getUrl());
+                if (recorder.checkUnfinished(bodyResult)) {
                     continue;
                 }
 
@@ -55,37 +46,36 @@ public class DiscShelfSpider {
                     Document document = Jsoup.parseBodyFragment(content);
                     Elements elements = document.select(".s-result-list.sg-row > div[data-asin]");
                     if (elements.size() > 0) {
-                        recorder.jmsSuccessRow("Found " + elements.size() + " data");
-                        parseDiscShelfs(elements, rowCount);
+                        recorder.jmsSuccessRow(task.getOrigin(), String.format("找到%d条数据", elements.size()));
+                        parseDiscShelfs(elements, recorder);
                     } else {
                         if (content.contains("api-services-support@amazon.com")) {
-                            recorder.jmsFailedRow("There seems to be an anti-robot system");
+                            recorder.jmsFailedRow("发现日亚反爬虫系统");
                         } else {
-                            recorder.jmsFailedRow("Data does not match the format");
+                            recorder.jmsFailedRow("页面数据未通过校验");
                         }
                     }
 
                 } catch (RuntimeException e) {
                     recorder.jmsErrorRow(e);
-                    writeContent(content, "page:" + recorder.getFetchCount());
+                    writeContent(content, "page=" + recorder.getFetchCount());
                 }
 
                 threadSleep(5);
             }
         });
 
-        jmsMessage.info("Found a total of %d new discs", rowCount.get());
+        jmsMessage.success("本次发现新碟片%个", recorder.getDataCount());
         recorder.jmsSummary();
         recorder.jmsEndUpdate();
     }
 
-    private void parseDiscShelfs(Elements elements, AtomicInteger rowCount) {
+    private void parseDiscShelfs(Elements elements, SpiderRecorder recorder) {
         elements.forEach(element -> {
             String asin = element.attr("data-asin");
             String title = element.select(".a-size-medium.a-color-base.a-text-normal").first().text();
             if (discShelfRepository.saveOrUpdate(asin, title)) {
-                jmsMessage.success(String.format("Find a new disc [asin=%s][title=%s]", asin, title));
-                rowCount.incrementAndGet();
+                recorder.jmsFoundData(String.format("发现新碟片[asin=%s][title=%s]", asin, title));
             }
         });
     }
