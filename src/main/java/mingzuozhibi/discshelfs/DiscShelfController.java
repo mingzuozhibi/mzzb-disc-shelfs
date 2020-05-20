496856551649327714c51910007704993371df71
fuhaiwei <fuhaiwei@126.com>
2020-05-20 17:51:49 +0800

    feat(action): Add Cookie Update

--- src/main/java/mingzuozhibi/discshelfs/DiscShelfController.java
+++ src/main/java/mingzuozhibi/discshelfs/DiscShelfController.java
@@ -1,5 +1,6 @@
 package mingzuozhibi.discshelfs;
 
+import lombok.SneakyThrows;
 import mingzuozhibi.common.BaseController;
 import mingzuozhibi.common.jms.JmsMessage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.List;
 
 import static mingzuozhibi.common.util.ThreadUtils.runWithDaemon;
@@ -50,6 +55,18 @@ public class DiscShelfController extends BaseController {
         Page<DiscShelf> resultPage = discShelfRepository.findAll(pageRequest);
         List<DiscShelf> resultList = resultPage.getContent();
         return objectResult(resultList, resultPage);
+    }
+
+    @JmsListener(destination = "cookie.update")
+    public void cookieUpdate(String cookie) {
+        try (PrintWriter writer = new PrintWriter(new File("amazon", "cookie"))) {
+            writer.println(cookie);
+            writer.flush();
+            jmsMessage.success("cookie 已更新");
+        } catch (Exception e) {
+            jmsMessage.warning("cookie 更新失败: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
 }
